dc25dbcc96193d36f0462d92e7b4f5f11dc5155e
goikhman.yuri <goikhman.yuri@9ccb431c-110a-11df-b192-4df06d6974c1>
2010-02-03 21:45:32 +0000

    init state

---
+++ modules/Notes/language/he_il.lang.php
@@ -0,0 +1,72 @@
+<?php
+/*********************************************************************************
+ * The contents of this file are subject to the SugarCRM Public License Version 1.1.2
+ * ("License"); You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at http://www.sugarcrm.com/SPL
+ * Software distributed under the License is distributed on an  "AS IS"  basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
+ * The Original Code is:  SugarCRM Open Source
+ * The Initial Developer of the Original Code is SugarCRM, Inc.
+ * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc.;
+ * All Rights Reserved.
+ * Contributor(s): ______________________________________.
+ ********************************************************************************/
+/*********************************************************************************
+ * $Header: /advent/projects/wesat/vtiger_crm/sugarcrm/modules/Notes/language/en_us.lang.php,v 1.10 2005/03/28 06:33:37 rank Exp $
+ * Description:  Defines the English language pack for the Account module.
+ * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc.
+ * All Rights Reserved.
+ * Contributor(s): ______________________________________..
+ ********************************************************************************/
+
+$mod_strings = Array(
+'LBL_MODULE_NAME'=>'הערות',
+'LBL_MODULE_TITLE'=>'הערות: בית',
+'LBL_SEARCH_FORM_TITLE'=>'חיפוש הערה',
+'LBL_LIST_FORM_TITLE'=>'רשימת הערות',
+'LBL_NEW_FORM_TITLE'=>'הערה חדשה',
+
+'LBL_LIST_SUBJECT'=>'נושא',
+'LBL_LIST_CONTACT_NAME'=>'שם איש קשר',
+'LBL_LIST_RELATED_TO'=>'קשור אל',
+'LBL_LIST_DATE_MODIFIED'=>'תאריך שינוי אחרון',
+
+'LBL_NOTE'=>'הערה:',
+'LBL_NOTE_SUBJECT'=>'נושא הערה:',
+'LBL_CONTACT_NAME'=>'שם איש קשר:',
+'LBL_PHONE'=>'טלפון:',
+'LBL_SUBJECT'=>'נושא:',
+'LBL_CLOSE'=>'Close:',
+'LBL_RELATED_TO'=>'קשור אל:',
+'LBL_EMAIL_ADDRESS'=>'כתובת דוא&quot;ל:',
+'LBL_COLON'=>':',
+
+'ERR_DELETE_RECORD'=>"A record number must be specified to delete the vtiger_account.",
+'LBL_LIST_FILENAME'=>"קובץ",
+
+// Added for Note(3.2-patch 3) 
+
+'LBL_FILENAME'=>'קובץ מצורף:',
+'LBL_NOTE_INFORMATION'=>'מידע הערה',
+
+// Added for 4GA
+'LBL_TOOL_FORM_TITLE'=>'כלי הערות',
+// Added for 4GA
+'Contact Name'=>'שם איש קשר',
+'Related To'=>'קשור אל',
+'Subject'=>'נושא',
+'Created Time'=>'שעת יצירה',
+'Modified Time'=>'שעת שינוי',
+'Attachment'=>'קובץ מצורף',
+'Note'=>'הערה',
+//Added for 4.2 Release -- CustomView
+'Related to'=>'קשור אל',
+'Last Modified'=>'תאריך שינוי אחרון',
+'File'=>'קובץ',
+'LBL_ALL'=>'הכל',
+'Title'=>'כותרת',
+
+);
+
+?>
