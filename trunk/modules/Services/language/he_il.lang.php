0dc787955034719dc6ec1bb520da11df968c87fb
goikhman.yuri <goikhman.yuri@9ccb431c-110a-11df-b192-4df06d6974c1>
2010-02-03 21:45:32 +0000

    init state

---
+++ trunk/modules/Services/language/he_il.lang.php
@@ -0,0 +1,59 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$mod_strings = Array(
+'LBL_MODULE_NAME'=>'שרותים',
+'SINGLE_Services'=>'שרות',
+'LBL_LIST_SERVICE_NAME'=>'שם השירות',
+'LBL_PRICEBOOK'=>'מחירון:',
+'LBL_PRICEBOOK_INFORMATION'=>'מידע על מחירון:',
+'LBL_PRICING_INFORMATION'=>'פירוט המחיר',
+'LBL_LIST_FORM_TITLE'=>'רשימת שרותים',
+'LBL_LIST_PRICEBOOK_FORM_TITLE'=>'רשימת מחירונים',
+'LBL_SERVICE_UNIT_PRICE'=>'מחיר יחידה',
+'LBL_PB_LIST_PRICE'=>'מחיר קטלוגי',
+'LBL_SERVICE_INFORMATION'=>'פירוט השרות',
+'LBL_CUSTOM_INFORMATION'=>'פירוט נוסף',
+'LBL_DESCRIPTION_INFORMATION'=>'תיאור מפורט',
+
+'LBL_ADD_PRICEBOOK_BUTTON_LABEL'=>'הוסף למחירונים',
+
+'Services'=>'שרותים',
+'Service Name'=>'שם השרות',
+'Service Active'=>'פעיל',
+'Service Category'=>'קטגוריה',
+'Service No'=>'מספר השרות',
+'Owner'=>'בעלים',
+'Days'=>'ימים',
+'Hours'=>'שעות',
+'Incidents'=>'אירועים',
+'No of Units'=>'מס יחידות',
+'Commission Rate'=>'(%) שיעור עמלה',
+'Unit Price'=>'מחיר יחידה',
+'Price'=>'מחיר',
+'Support Start Date'=>'תאריך התחלת תמיכה',
+'Support Expiry Date'=>'תאריך תפוגת תמיכה',
+'Sales Start Date'=>'תאריך התחלת מכירות',
+'Sales End Date'=>'תאריך סיום מכירות',
+'Created Time'=>'תאריך היצירה',
+'Modified Time'=>'תאריך שינוי',
+'Related To'=>'קשור ל-',
+'Usage Unit'=>'יחידות השימוש',
+'Currency'=>'מטבע',
+'Tax Class'=>'שיעור המס',
+'Website'=>'אתר אינטרנט',
+'Description'=>'תיאור',
+
+'Price Book Name'=>'שם מחירון',
+
+'LBL_NO_TAXES_ASSOCIATED'=>'אין מסים המשויכים לשירות זה',
+);
+
+?>
